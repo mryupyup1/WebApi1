bba43292bb83a6246e0639c5973456a6a2406b1d
aman pratap singh <singhamanpratap61@gmail.com>
2022-02-07 15:11:35 +0530

    Add project files.

---
+++ WebApi1/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi1.Model;
+
+namespace WebApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         
+        static List<Employee> _employee = null;
+        void Initialize()
+        {
+            _employee = new List<Employee>()
+           {
+               new Employee() { Id=1, Name="Arav" , Address= "Delhi"},
+               new Employee() { Id=2, Name="Rohit" , Address= "Pune"},
+
+
+           };
+
+        }
+        public EmployeeController()
+        {
+            if (_employee == null)
+            {
+                Initialize();
+            }
+        }
+
+
+        
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _employee;
+        }
+
+        
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            Employee emp = _employee.Where(x => x.Id == id).FirstOrDefault();
+            return emp;
+        }
+    }
+}
